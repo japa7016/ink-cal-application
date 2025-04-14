3f6b6c54fc2937768e9fa136334be781cbe91f70
Parth Patel <papa9185@colorado.edu>
2025-04-14 00:09:03 -0600

    added reference epdconfig.py

--- epaper_driver/epdconfig.py
+++ epaper_driver/epdconfig.py
@@ -26,7 +26,7 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 # THE SOFTWARE.
 #
-
+# referenced from https://github.com/waveshareteam/e-Paper
 import os
 import logging
 import sys
